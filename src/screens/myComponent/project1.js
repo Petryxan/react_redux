46469398284e0d3a0b1610185ca193061e8abb08
unknown <petryxan@seznam.cz>
2020-05-03 09:44:20 +0300

    modified myComponents

---
+++ src/screens/myComponent/project1.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Project1 = () => {
+    return(
+        <div>
+            Project1
+        </div>
+    )
+}
+export default Project1
--- src/screens/myComponents.js
+++ src/screens/myComponents.js
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+// import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 import styles from "../styles/MyComponentsStyles.module.css";
-
+import Project1 from "./myComponent/project1";
+import Project2 from "./myComponent/project2";
 
 export const MyComponents = () => {
-  return (
-    <div className={styles.container}>
-    My test Component Page
+  const [project, setProject] = useState(false);
+  const [project2, setProject2] = useState(false);
+  // const [project3, setProject3] = useState(false);
+  // const [project4, setProject4] = useState(false);
 
-  
+  return (
+    <div className={styles.MyComponents}>
+      <div className={styles.menu}>
+        <li>
+         
+          <button onClick={() => setProject(!project)}>Project 1</button>
+        </li>
+        <li>
+       
+          <button onClick={() => setProject2(!project2)}>Project 2</button>
+        </li>
+        <li>Project 3</li>
+        <li>Project 4</li>
+        <li>Project 5</li>
+        <li>Project 6</li>
+        {/* <Switch> */}
+
+        {/* { this.state.isLoggedIn ? <Route path="/about" component={About}/> : null } */}
+
+        {/* <Route path="/myComponents/project1" component={Project1}/> */}
+        {/* <Route path="/cars" component={Cars}/> */}
+        {/* <Redirect to={'/'}/> */}
+        {/*<Route render={() => <h1 style={{color: 'red', textAlign: 'center'}}>404 not found</h1>} />*/}
+        {/* </Switch> */}
+      </div>
+      <div className={styles.main}>
+        <div>
+          {project ? <Project1 /> : null}
+          {project2 ? <Project2 /> : null}
+        </div>
+      </div>
     </div>
   );
 };
